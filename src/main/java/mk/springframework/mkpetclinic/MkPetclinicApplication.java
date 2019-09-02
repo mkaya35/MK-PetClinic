8a01c79381a9e961eeb1c00f8c28442378ed91c8
mkaya35 <mevomevo35>
2019-09-02 11:24:47 -0400

    initial project add

---
+++ src/main/java/mk/springframework/mkpetclinic/MkPetclinicApplication.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/springframework/mkpetclinic/MkPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
